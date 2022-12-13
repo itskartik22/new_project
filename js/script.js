f39d9e78b906dacbc3c41c2d0fa60143755e2812
Kartik Kumar <thakurkartik2262@gmail.com>
2022-12-13 14:42:37 +0530

    x-mark inclusion

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 
-var navBtn = document.getElementById("sideOpen");
-
-navBtn.onclick = function(){
-    document.body.classList.toggle("nav-close");
-    if(document.body.classList.contains("nav-close")){
-        navBtn.firstElementChild.classList.replace("fa-solid","fa-regular");
-        navBtn.firstElementChild.classList.replace("fa-bars","fa-xmark");
-    }
-    else{
-        navBtn.firstElementChild.classList.replace("fa-regular","fa-solid");
-        navBtn.firstElementChild.classList.replace("fa-xmark","fa-bars");
-    }
+var navOpen = document.getElementById("sideOpen");
+var navClose = document.getElementById("sideClose")
+function navSlide() {
+    document.body.classList.toggle("nav-open");
 }
